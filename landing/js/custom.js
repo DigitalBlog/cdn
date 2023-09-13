cb9f152ad5a82f9bdb8f76706f0c497649c46eb1
ITQ <118541411+DevItq@users.noreply.github.com>
2023-09-13 17:25:19 +0300

    Update custom.js

--- landing/js/custom.js
+++ landing/js/custom.js
@@ -37,16 +37,16 @@ $(window).on('load', function(){
 	/* ========================================================== */
 	/*   SmoothScroll                                             */
 	/* ========================================================== */
-	
-	$(".navbar-nav .scroll li a, a.scrool").on('click', function(e) {
-		
-		var full_url = this.href;
-		var parts = full_url.split("#");
-		var trgt = parts[1];
-		var target_offset = $("#"+trgt).offset();
-		var target_top = target_offset.top;
-		
-		$('html,body').animate({scrollTop:target_top -70}, 1000);
-			return false;
-		
-	});	
+
+$(".navbar-nav li a, a.scroll").on('click', function(e) {
+  if ($(this).hasClass('scrollable')) {
+    var full_url = this.href;
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+    var target_offset = $("#" + trgt).offset();
+    var target_top = target_offset.top;
+
+    $('html,body').animate({ scrollTop: target_top - 70 }, 1000);
+    return false;
+  }
+});
