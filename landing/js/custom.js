1328669202d268cbb74c33544780b2f6f9fe1f7e
ITQ <118541411+DevItq@users.noreply.github.com>
2023-09-15 08:22:26 +0300

    Update custom.js

--- landing/js/custom.js
+++ landing/js/custom.js
@@ -14,14 +14,34 @@ $(window).on('load', function(){
 		filter: ':not(.external)'
 	});
 	$(".navbar-nav li a, a.scroll").on('click', function(e) {
-	  if ($(this).hasClass('scrollable')) {
-	    var full_url = this.href;
-	    var parts = full_url.split("#");
-	    var trgt = parts[1];
-	    var target_offset = $("#" + trgt).offset();
-	    var target_top = target_offset.top;
-	    $('html,body').animate({ scrollTop: target_top - 70 }, 1000);
-	    return false;
-	  }
-});
+		if ($(this).hasClass('scrollable')) {
+			var full_url = this.href;
+			var parts = full_url.split("#");
+			var trgt = parts[1];
+			var target_offset = $("#" + trgt).offset();
+			var target_top = target_offset.top;
+			$('html,body').animate({ scrollTop: target_top - 70 }, 1000);
+			return false;
+		}
+	});
+	$('#contact-form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'names':$('input[name="contact_names"]').val(),
+					'subject':$('input[name="contact_subject"]').val(),
+					'email':$('input[name="contact_email"]').val(),
+					'phone':$('input[name="contact_phone"]').val(),
+					'message':$('textarea[name="contact_message"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	})
 });
