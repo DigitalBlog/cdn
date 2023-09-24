695ae5a6c48e6858df07d89f70c6d60a256600cf
ITQ <118541411+DevItq@users.noreply.github.com>
2023-09-24 14:34:16 +0300

    Update preview.js

--- js/preview.js
+++ js/preview.js
@@ -1,7 +1,7 @@
 tinymce.init({
   selector: '#preview',
   language: 'ru',
-  plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+  plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount charmap quickbars emoticons',
   menubar: false,
   toolbar: false,
   readonly: true,
